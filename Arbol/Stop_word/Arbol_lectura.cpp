8bbfa66c1a81a9fcdd094d10f4ca23bcbf6a011f
Gonzalo Caira <gc98wootz@gmail.com>
2018-09-25 18:16:40 -0500

    Terminada 50

--- Arbol/Stop_word/Arbol_lectura.cpp
+++ Arbol/Stop_word/Arbol_lectura.cpp
@@ -13,103 +13,159 @@ using namespace sf;
 using namespace std;
 
 using namespace std;
-
 template <class T>
 class Nodo{
 public:
-  T m_pDato[1];
-  int frequency;
+  T m_pDato[2];
   Nodo <T>* m_pSon[2];
-  //Node <T> * right;
-  //Node <T> * parent;
+  int frecuencia;
 public:
-  Nodo (T word){
-    //parent=0;
+  Nodo (T english, T spanish){
     m_pSon[0]=m_pSon[1]=0;
-    m_pDato[0]=word;
-    frequency=1;
+    m_pDato[0]=english;
+    m_pDato[1]=spanish;
+    frecuencia=1;
+  }
+};
+
+//Nodo stop word
+template <class T>
+class Nodo_S{
+public:
+  T m_pDato;
+  Nodo_S <T>* m_pSon[2];
+public:
+  Nodo_S (T stop_word){
+    m_pSon[0]=m_pSon[1]=0;
+    m_pDato=stop_word;
   }
 };
 
 template <class T>
 class Tree{
-  //typedef Node<T>  * pNodo;
-  //typedef Node<T>  ** pdNodo;
-  //  private:
 public:
-      Nodo<T> * m_pRoot;
-      Nodo<T> * m_maxFrequency;
+      //Nodo<T> * m_pFrecuencia;
+      Nodo<T> * m_pRoot_text;
+      Nodo_S<T> * m_pRoot_stop_word;
+      Nodo<T> *max_fre;
       vector<vector<string> > myvector;
-      T max;
+      vector<string> myvector_stop;
     public:
       Tree(){
-        m_pRoot=0;
-        m_maxFrequency=0;
+        m_pRoot_text=0;
+        m_pRoot_stop_word=0;
+        max_fre=0;
       }
 
-      //leer palabras del texto
-      void read_vector_stopwords (){
+      //leer palabras del diccionario
+      void read_vector (/*vector<vector<string> > &myvector*/){
+        string line;
+        ifstream myfile ("SPANISH.TXT");
+        if (myfile.is_open())
+        {
+          while ( getline (myfile,line) ){
+            vector<string> vector_word;
+            istringstream buf(line);
+            //added
+            int i=0;
+            string word_second="";
+            //..
+            for (string word ;buf>>word; i++){
+                if (i==0) vector_word.push_back(word);
+                word_second=word_second+word+" ";
+            }
+            vector_word.push_back(word_second);
+            //cout<<word_second<<endl;
+            myvector.push_back(vector_word);
+          }
+          myfile.close();
+        }
+        else cout << "No se puedo abrir el archivo"<<endl;
+      }
+/*
+      bool Find_stop (T d, Nodo_S <T> **&p){
+        p=&m_pRoot_stop_word;
+        while (*p){
+          if ((*p)->m_pDato==d){
+            //frecuencia+=1;
+            return true;
+          }
+            //pp=(*p);
+            p=&(*p)->m_pSon[(*p)->m_pDato<d];
+        }
+        return false;
+      }
+      bool Add_stop (T word){
+        Nodo_S <T> **q;
+        //pq=new Nodo<T>(0);
+        if (Find_stop(word,q)) return false;
+        *q=new Nodo_S<T>(word);
+        //(*q)->parent=pq;
+        return true;
+      }*/
+      void read_vector_word (/*vector<vector<string> > &myvector*/){
+        string line;
+        ifstream myfile ("SPANISH.TXT");
+        if (myfile.is_open())
+        {
+          while ( getline (myfile,line) ){
+            Nodo_S <T> **q;
+            vector<string> vector_word;
+            istringstream buf(line);
+            //added
+            int i=0;
+            string word_second="";
+            //..
+            for (string word ;buf>>word; ){
+                if (!Find_stop(word,q)){
+                if (i==0) vector_word.push_back(word);
+                word_second=word_second+word+" ";
+                i++;}
+            }
+            vector_word.push_back(word_second);
+            //cout<<word_second<<endl;
+            myvector.push_back(vector_word);
+          }
+          myfile.close();
+        }
+        else cout << "No se puedo abrir el archivo"<<endl;
+      }
+
+      //Read stop words
+      void read_stop_words (/*vector<vector<string> > &myvector*/){
         string line;
         ifstream myfile ("stopwords.txt");
         if (myfile.is_open())
         {
           while ( getline (myfile,line) ){
-            vector<string> vector_word;
-            istringstream buf(line);
-            //added
-            int i=0;
-            string word_second="";
-            //..
-            for (string word ;buf>>word; i++){
-                if (i==0) vector_word.push_back(word);
-                word_second=word_second+word+" ";
-            }
-            vector_word.push_back(word_second);
-            //cout<<word_second<<endl;
-            myvector.push_back(vector_word);
+            myvector_stop.push_back(line);
           }
           myfile.close();
         }
         else cout << "No se puedo abrir el archivo"<<endl;
       }
-      //leer palabras del texto
-      void read_vector_text (){
-        string line;
-        //ifstream myfile ("FabulasDeSamaniego.txt");
-        //ifstream myfile ("Bishop - Pattern Recognition And Machine Learning - Springer  2006.txt");
-        ifstream myfile ("Bishop - Pattern Recognition And Machine Learning - Springer  2006.txt");
-        if (myfile.is_open())
-        {
-          while ( getline (myfile,line) ){
-            vector<string> vector_word;
-            istringstream buf(line);
-            //added
-            int i=0;
-            string word_second="";
-            //..
-            for (string word ;buf>>word; i++){
-                if (i==0) vector_word.push_back(word);
-                word_second=word_second+word+" ";
-            }
-            vector_word.push_back(word_second);
-            //cout<<word_second<<endl;
-            myvector.push_back(vector_word);
-          }
-          myfile.close();
+
+        /*for (int i=0;i<myvector.size();i++){
+          cout<<myvector[i][0]<<"  "<<myvector[i][1]<<endl;
         }
-        else cout << "No se puedo abrir el archivo"<<endl;
-      }
+      }*/
+      /*  vector<string> vector_word;
+        istringstream buf(line);
+        int i=0;
+        string word_second=" ";
+        for (string word;buf>>word; i++){
+          if (i==0) vector_word.push_back(word);
+          word_second=word_second+word+" ";
+        }
+        vector_word.push_back(word_second);
+        myvector.push_back(vector_word);
+      }*/
 
       bool Find (T d, Nodo <T> **&p){
-        p=&m_pRoot;
+        p=&m_pRoot_text;
         while (*p){
-          if ((*p)->m_pDato[0]==d) {
-
-            //añade frequencia a la palabra
-            (*p)->frequency++;
-            if((*p)->frequency>m_maxFrequency->frequency){
-              m_maxFrequency=(*p);
-            }
+          if ((*p)->m_pDato[0]==d){
+            (*p)->frecuencia+=1;
             return true;
           }
             //pp=(*p);
@@ -117,13 +173,9 @@ public:
         }
         return false;
       }
-
-
-
-      /*
       bool Find_translate (T d){
         Nodo <T> **p;
-        p=&m_pRoot;
+        p=&m_pRoot_text;
         while (*p){
           if ((*p)->m_pDato[0]==d) {
             cout<<"Translte "<<(*p)->m_pDato[1]<<endl;
@@ -135,20 +187,49 @@ public:
         }
         //delete p;
         return false;
-      }*/
-      bool Add (T word){
+      }
+      bool Add (T eng, T spa){
         Nodo <T> **q;
         //pq=new Nodo<T>(0);
-        if (Find(word,q)) return false;
-        *q=new Nodo<T>(word);
+        if (Find(eng,q)) return false;
+        *q=new Nodo<T>(eng,spa);
         //(*q)->parent=pq;
         return true;
+      }
+
+      bool Find_stop (T d, Nodo_S <T> **&p){
+        p=&m_pRoot_stop_word;
+        while (*p){
+          if ((*p)->m_pDato==d){
+            //frecuencia+=1;
+            return true;
+          }
+            //pp=(*p);
+            p=&(*p)->m_pSon[(*p)->m_pDato<d];
+        }
+        return false;
+      }
+
+      bool Add_stop (T word){
+        Nodo_S <T> **q;
+        //pq=new Nodo<T>(0);
+        if (Find_stop(word,q)) return false;
+        *q=new Nodo_S<T>(word);
+        //(*q)->parent=pq;
+        return true;
+      }
+      void Print_s (Nodo_S <T> **p){
+        if (!(*p)) return;
+        //cout<<"padre: "<<((*p)->parent)->m_pDato<<endl;
+        cout<<(*p)->m_pDato<<endl;
+        Print_s (&(*p)->m_pSon[0]);
+        Print_s (&(*p)->m_pSon[1]);
       }
 
       bool Find_father (T d){
         Nodo <T> *padre_q;
         Nodo <T> *q;
-        q=padre_q=m_pRoot;
+        q=padre_q=m_pRoot_text;
         while (q){
           if (q->m_pDato[0]==d) break;
             padre_q=(q);
@@ -158,41 +239,23 @@ public:
         cout<<"padre "<<padre_q->m_pDato[0]<<endl;
         return true;
       }
-      void getMaxfrequency(){
-        cout<<m_maxFrequency->frequency<<endl;
-      }
 
-      /*bool sibling (T d){
-        Node <T> **q;
-        Node <T> *pq;
-        if (Find(d,q,pq)) return false;
-        *q=new Node<T>(d);
-        (*q)->parent=pq;
-        return true;
-      }*/
-      void MaxFrequency(){
-        Nodo<T> *tmp=m_pRoot;
-        int max=-1;
-        if(tmp)max=tmp->frequency;
-        /*while(tmp){
-
-        }*/
-        cout<<tmp->m_pDato[0];
-        cout<<tmp->frequency<<endl;
-      }
       void Print (Nodo <T> **p){
         if (!(*p)) return;
         //cout<<"padre: "<<((*p)->parent)->m_pDato<<endl;
-        cout<<"Nodo: "<<(*p)->m_pDato[0]<<" "<<(*p)->frequency<<endl;
-
+        cout<<(*p)->m_pDato[0]<<" "<<(*p)->m_pDato[1]<<endl;
         Print (&(*p)->m_pSon[0]);
-
         Print (&(*p)->m_pSon[1]);
       }
-      void show(){
-        Nodo<T> **tmp=&m_pRoot;
-        Print(tmp);
-      }
+      /*void Print (){
+        if (!(*p)) return;
+        //cout<<"padre: "<<((*p)->parent)->m_pDato<<endl;
+        while (*P){
+        cout<<"Nodo: "<<(*p)->m_pDato[0]<<endl;
+        Print (&(*p)->m_pSon[0]);
+        Print (&(*p)->m_pSon[1]);
+        }
+      }*/
       int Quantity (Nodo <T> *p){
         if (p) return 1+Quantity (p->m_pSon[1])+Quantity (p->m_pSon[0]);
         else return 0;
@@ -202,12 +265,11 @@ public:
           if (!p) return 0;
           return max(Altura_Arbol(p->m_pSon[0]),Altura_Arbol(p->m_pSon[1]))+1;
       }
+
 };
 
-
 int main(int argc, char const *argv[]) {
-  Tree <string> Arbol_stopwords;
-  Tree <string> Arbol_txt;
+  Tree <string> Arbol;
 //  Agenda<string> A;
   RenderWindow window(VideoMode(500,400),"Stopwords :D");
   window.setVerticalSyncEnabled(true);
@@ -285,7 +347,7 @@ int main(int argc, char const *argv[]) {
                 switch (menu.GetPressedItem()) {
                   case 0:
                       //añadir al arbol el texto,pero las palabras de stopword
-                      Arbol_stopwords.read_vector_stopwords();
+                      /*Arbol_stopwords.read_vector_stopwords();
                       for (int i=0; i<Arbol_stopwords.myvector.size();i++){
                           Arbol_stopwords.Add(Arbol_stopwords.myvector[i][0]);
                       }
@@ -293,16 +355,29 @@ int main(int argc, char const *argv[]) {
                       for (int i=0; i<Arbol_txt.myvector.size();i++){
                         //if(Arbol_txt.myvector[i][0])
                           Arbol_txt.Add(Arbol_txt.myvector[i][0]);
+                      }*/
+                      Arbol.read_vector();
+                      for (int i=0; i<Arbol.myvector.size();i++){
+                          Arbol.Add(Arbol.myvector[i][0],Arbol.myvector[i][1]);
                       }
+
+                      Arbol.read_stop_words();
+                      for (int i=0; i<Arbol.myvector_stop.size();i++){
+                          Arbol.Add_stop(Arbol.myvector_stop[i]);
+                      }
+
+                      Arbol.read_vector_word();
+                      Arbol.Print(&Arbol.m_pRoot_text);
+
 
                     break;
                   case 1:
                       //mostrar el de mayor frecuencia
-                      Arbol_txt.getMaxfrequency();
-                      q="oasd";
+                      //Arbol_txt.getMaxfrequency();
+                      q="Tree = ";
                       text3.setString(q);
-                      text3.setCharacterSize(100);
-                      text3.setPosition(40,120);
+                      text3.setCharacterSize(50);
+                      text3.setPosition(40,150);
                       text3.setFillColor(Color::Blue);
                       //window.clear();
                       window.draw(text3);
