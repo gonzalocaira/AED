1ad517a9fd128f71f6041130a95988fd38aeebf2
Gonzalo Caira <gc98wootz@gmail.com>
2018-11-27 10:26:50 -0500

    delete primt

--- Grafos/grafo.cpp
+++ Grafos/grafo.cpp
@@ -97,42 +97,7 @@ public:
         }
         return false;
     }
-    vector <vector<int> > prim(){
-        // uso una copia de ady porque necesito eliminar columnas
-    vector< vector<int> > adyacencia = this->ady;
-    vector< vector<int> > arbol(cn);
-    vector<int> markedLines;
-    vector<int> :: iterator itVec;
-
-    // Inicializo las distancias del arbol en INF.
-    for(int i = 0; i < cn; i++)
-        arbol[i] = vector<int> (cn, INF);
-
-    int padre = 0;
-    int hijo = 0;
-    while(markedLines.size() + 1 < cn){
-        padre = hijo;
-        // Marco la fila y elimino la columna del nodo padre.
-        markedLines.push_back(padre);
-        for(int i = 0; i < cn; i++)
-            adyacencia[i][padre] = INF;
-
-        // Encuentro la menor distancia entre las filas marcadas.
-        // El nodo padre es la linea marcada y el nodo hijo es la columna del minimo.
-        int min = INF;
-        for(itVec = markedLines.begin(); itVec != markedLines.end(); itVec++)
-            for(int i = 0; i < cn; i++)
-                if(min > adyacencia[*itVec][i]){
-                    min = adyacencia[*itVec][i];
-                    padre = *itVec;
-                    hijo = i;
-                }
-
-        arbol[padre][hijo] = min;
-        arbol[hijo][padre] = min;
-    }
-    return arbol;
-    }
+    
     void GraphViz(){
         string title = "Grafo";
  		ofstream archivo("Grafo.dot");
